519856ada0ff12836383509624e03ab7b96bcafe
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-06-27 12:41:31 +0200

    on the lesson

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import PizzaComponent from './components/PizzaComponent/PizzaComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <PizzaComponent name="kolbászos rájás" price="1500" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
