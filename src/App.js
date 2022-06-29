5b15c15b81739db02c83f9e44c2c6e26bd727f9c
NemethSzegediJudit <szegedijudit.gyp@gmail.com>
2022-06-29 18:06:44 +0200

    wednesday lesson

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import PizzaComponent from './components/PizzaComponent/PizzaComponent';
+//import Timer2 from './components/Timer2';
+import Timer from './components/Timer';
+import { useState } from 'react';
 
 function App() {
+  const [showTimer, setShowTimer] = useState(true)
+  //a fenti sor alább kifejtve:
+  //const state = useState(true)
+  //const showTimer = state[0]
+  //const setShowTimer = state[1]
+
+  //a showtimer az állapotot adja vissza,a setshowtimer pedig a függvényt, ami megváltoztatja az állapotot
+
+  if (5 < 3 && 10 < 20) {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <PizzaComponent name="kolbászos rájás" price="1500" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {showTimer ? <Timer /> : <div>no timer</div>}
+        <button onClick={() => {setShowTimer(false)}}>hide</button>
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/PizzaComponent/PizzaComponent.js
+++ src/components/PizzaComponent/PizzaComponent.js
@@ -1,20 +1,3 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './PizzaComponent.css';
-
-const PizzaComponent = (props) => {
-  const name = props.name;
-  const price = props.price;
-
-  return (
-    <div className="PizzaComponent">
-      Név:{name}
-      Ár: {price}
-    </div>
-)};
-
-PizzaComponent.propTypes = {};
-
-PizzaComponent.defaultProps = {};
-
-export default PizzaComponent;
